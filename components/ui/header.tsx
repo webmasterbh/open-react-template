9abbf39b6ee1b218a733da7d40b7bc343e8414fa
Wanderson <bhwsouza@gmail.com>
2023-07-29 21:04:43 -0300

    test

--- components/ui/header.tsx
+++ components/ui/header.tsx
@@ -29,6 +29,22 @@ export default function Header() {
                 </Link>
               </li>
               <li>
+                <Link
+                  href="/signin"
+                  className="font-medium text-purple-600 hover:text-gray-200 px-4 py-3 flex items-center transition duration-150 ease-in-out"
+                >
+                  Sign in
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="/signin"
+                  className="font-medium text-purple-600 hover:text-gray-200 px-4 py-3 flex items-center transition duration-150 ease-in-out"
+                >
+                  Sign in
+                </Link>
+              </li>
+              <li>
                 <Link href="/signup" className="btn-sm text-white bg-purple-600 hover:bg-purple-700 ml-3">
                   Sign up
                 </Link>
